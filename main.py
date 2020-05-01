5bd06786adf8e8d5ed62a2b74b1af763d90cd9fa
tomisinteazer <tomisinteazer@gmail.com>
2020-05-01 08:13:23 +0100

    writer created

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 def main():
   print('the python is alive')
+  file = open('new.txt', 'w+')
+  print('file opened enter new text')
+  new = input()
+  file.write(new)
+  file.close()
+  print('file written success')
 
 main()
