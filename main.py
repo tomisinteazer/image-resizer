bf4d1482acfe3beac576e20105fb714f7747193e
tomisinteazer <tomisinteazer@gmail.com>
2020-05-01 08:17:59 +0100

    updated writer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
   print('the python is alive')
-  file = open('new.txt', 'w+')
+  file = open('new.txt', 'a+')
   print('file opened enter new text')
   new = input()
   file.write(new)
