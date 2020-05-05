b71c38ad27d07d6b21c3dd5e534bfbfbcb6b696c
tomisinteazer <tomisinteazer@gmail.com>
2020-05-05 14:56:06 +0100

    new test

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 def main():
-  print('the python writer is alive')
-  file = open('new.txt', 'a+')
-  print('file opened enter text')
-  new = input()
-  edited = new +'\n'
-  file.write(edited)
-  file.close()
-  print('file updated successfully')
+  def writer():
+    print('the python writer is alive')
+    file = open('new.txt', 'a+')
+    print('file opened enter text')
+    new = input()
+    edited = new +'\n'
+    file.write(edited)
+    file.close()
+    print('file updated successfully')
   
   
   #the prime number generator
   num = 7
   def prime(num):
+    isprime=false
     for i in range(1,num):
       if num%i!=0:
-      print('it is prime')
+        isprime = True
+      else:
+        isprime = False
+        break 
+    print(isprime)
         # TODO: write code...
       # TODO: write code...
   prime(num)
