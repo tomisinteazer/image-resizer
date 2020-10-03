82d41f1e2f196f02832b4b40349460419d373066
Tomisinteazer <tomisinteazer@.com>
2020-10-03 01:57:14 +0100

    cleaned up

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # image-resizer
 
-Image manipulation engine using jimp to bulk resize images
+Image manipulation using jimp to bulk resize images
+create both an input and output folder
 put immages in the input folder then run check your output in the output folder
--- resize.js
+++ resize.js
@@ -3,29 +3,29 @@ let jimp = require("jimp");
 let files = fs.readdirSync("./input");
 let images = [];
 for (file of files) {
-    images.push(file);
-
+  images.push(file);
 }
-let batch = images.map(e => `input/${e}`);
-
+let batch = images.map((e) => `input/${e}`);
+batch.shift();
 let jimps = [];
 for (e of batch) {
-    jimps.push(jimp.read(e))
+  jimps.push(jimp.read(e));
 }
 
-Promise.all(jimps).then(() => {
-    return Promise.all(jimps)
-}).then(data => {
+Promise.all(jimps)
+  .then(() => {
+    return Promise.all(jimps);
+  })
+  .then((data) => {
     for (info = 0; info < data.length; info++) {
-
-        let newimg = data[info];
-        newimg.resize(400, 400);
-        newimg.write(`output/${info}.jpeg`);
-
+      let newimg = data[info];
+      newimg.resize(400, 400);
+      newimg.write(`output/${info}.jpeg`);
     }
     newFunction();
-})
+  })
+  .catch((e) => console.log(e));
 
 function newFunction() {
-    console.log("i have finished master");
+  console.log("i have finished master");
 }
